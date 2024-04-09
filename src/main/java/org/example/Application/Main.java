e3da342af0ed72d9c283a48c9548381e68d17936
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2024-04-09 19:49:12 -0300

    criando as classes

--- src/main/java/org/example/Application/Main.java
+++ src/main/java/org/example/Application/Main.java
@@ -2,4 +2,21 @@ package org.example.Application;
 
 public class Main {
 
+    public static void main(String[] args) {
+        System.out.println(
+                        "██╗      ██╗   ██╗██████╗  ██████╗ \n" +
+                        "██║      ██║   ██║██╔══██╗██╔═══██╗\n" +
+                        "██║      ██║   ██║██║  ██║██║   ██║\n" +
+                        "██║      ██║   ██║██║  ██║██║   ██║\n" +
+                        "███████╗ ╚██████╔╝██████╔╝╚██████╔╝\n" +
+                        "╚══════╝  ╚═════╝ ╚═════╝  ╚═════╝ \n"
+        );
+
+        System.out.println("Bem vindo ao jogo de tabuleiro!");
+
+
+
+
+    }
+
 }
